df7321ec52f204fb41a9f61be097d71cf1499002
savvydarknight <153359624+savvydarknight@users.noreply.github.com>
2026-06-26 14:18:05 +0300

    Update config.js

--- config.js
+++ config.js
@@ -23,8 +23,8 @@ module.exports = {
 
   probe: {
     timeoutSeconds:     13,
-    retries:            2,
-    retryDelaySeconds:  3,
+    retries:            1,
+    retryDelaySeconds:  2,
     concurrency:        25,
     segmentConcurrency: 8,
     youtubeConcurrency: 6,
