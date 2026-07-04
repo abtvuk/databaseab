978e3259a5c576a2035959408a9c541cb286c954
savvydarknight <153359624+savvydarknight@users.noreply.github.com>
2026-07-04 09:47:25 +0300

    Increase retries from 1 to 2 in probe configuration

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ module.exports = {
 
   probe: {
     timeoutSeconds:     15,
-    retries:            1,
+    retries:            2,
     retryDelaySeconds:  3,
     concurrency:        25,
     segmentConcurrency: 8,
