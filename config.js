7b00884516fe2129c841e98886df08bd343c5c83
savvydarknight <153359624+savvydarknight@users.noreply.github.com>
2026-07-07 16:31:30 +0300

    chore: update config.js via admin UI

--- config.js
+++ config.js
@@ -105,6 +105,10 @@ module.exports = {
     'flv1', 'rv10', 'rv20', 'rv30', 'rv40',
   ],
 
-  criticalChannels: ['cna.dz', 'live.elheddaftv.com'],
+  criticalChannels: [
+    'cna.dz',
+    'live.elheddaftv.com',
+    'livesstream.work.gd',
+  ],
 
 }
